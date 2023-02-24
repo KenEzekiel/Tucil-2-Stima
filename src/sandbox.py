2bc68824aa1274a4f8a0c7a0e747447db0aeb9fb
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2023-02-25 00:48:55 +0700

    modified sandbox

--- src/sandbox.py
+++ src/sandbox.py
@@ -1,10 +1,14 @@
 from Handler.InputHandler import InputHandler
+import MathService as Mt
 
 print("File : \t Input Handler tester")
 
 I1 = InputHandler(3, 4, 100, decimalOn=False)
-I2 = InputHandler(3, 3, 10)
+I2 = InputHandler(5, 3, 10)
 
 I1.printVectors()
 I2.printVectors()
-I2.visualizeVectors()
+# I2.visualizeVectors()
+
+pair, dist = Mt.getClosestPair(I2.vecArr, I2.num)
+print(pair, dist)
